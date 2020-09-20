506b801b33b8513ceac44af6974cf37bd31850f6
Wameed Miriako <wameed.miriako@wayne.edu>
2020-09-19 21:11:22 -0400

    Adding author email

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Author: Wameed Miriako
+// Author: Wameed Miriako <wameed.miriako@wayne.edu>
 
 // Header file for input output functions 
 #include<iostream>  
